6568c0348aa7c5f702527f3a80ca350c371daeb5
FredaWerdiger <fwerdiger@gmail.com>
2023-08-10 13:26:05 +1000

    This now works for 3D images. Code only rotates in the axial plane.

--- image_3D_align.py
+++ image_3D_align.py
@@ -1,10 +1,59 @@
+import sys
 import SimpleITK as sitk
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
+import os
+#
+# def rotate_and_resample(im, roll, pitch, yaw, show=False):
+#     arr = sitk.GetArrayFromImage(im)
+#     z, y, x = np.nonzero(arr)
+#     mean_z = np.mean(z)
+#     mean_x = np.mean(x)
+#     mean_y = np.mean(y)
+#
+#     # ROTATING
+#     rotation_centre = (int(np.ceil(mean_x)), int(np.ceil(mean_y)), int(np.ceil(mean_z)))
+#     rotation_centre_im = im.TransformIndexToPhysicalPoint(rotation_centre)
+#
+#     rigid_euler = sitk.Euler3DTransform()
+#     rigid_euler.SetRotation(roll, pitch, yaw)
+#
+#     rigid_versor = sitk.VersorRigid3DTransform()
+#     rigid_versor.SetMatrix(rigid_euler.GetMatrix())
+#     rigid_versor.SetCenter(rotation_centre_im)
+#
+#     # RESAMPLING
+#     interpolator = sitk.sitkNearestNeighbor
+#     default_value = 0
+#     resampled = sitk.Resample(
+#         im,
+#         im,
+#         rigid_versor,
+#         interpolator,
+#         default_value
+#     )
+#
+#     resampled = sitk.Cast(resampled, sitk.sitkFloat32)
+#
+#     if show:
+#         new_array = sitk.GetArrayFromImage(resampled)
+#         fig, ax = plt.subplots(1, 3, figsize=(10, 3))
+#         axial_slice_num = int(input('Enter axial slice number you wish to see:'))
+#         coronal_slice_num = int(input('Enter coronal slice number:'))
+#         sagittal_slice_num = int(input('Enter sagittal slice number:'))
+#         ax[0].imshow(new_array[axial_slice_num])
+#         ax[0].set_title('xy')
+#         ax[1].imshow(new_array[:, coronal_slice_num, :], interpolation='nearest', aspect='auto')
+#         ax[1].set_title('xz')
+#         ax[2].imshow(new_array[:, :, sagittal_slice_num], interpolation='nearest', aspect='auto')
+#         ax[2].set_title('yz')
+#         plt.show()
+#
+#     return resampled
 
 
-def rotate_and_resample(im, roll, pitch, yaw, show=False):
+def rotate_and_resample_yaw(im, z_slice, yaw, show=False):
     arr = sitk.GetArrayFromImage(im)
     z, y, x = np.nonzero(arr)
     mean_z = np.mean(z)
@@ -16,7 +65,7 @@ def rotate_and_resample(im, roll, pitch, yaw, show=False):
     rotation_centre_im = im.TransformIndexToPhysicalPoint(rotation_centre)
 
     rigid_euler = sitk.Euler3DTransform()
-    rigid_euler.SetRotation(roll, pitch, yaw)
+    rigid_euler.SetRotation(0, 0, yaw)
 
     rigid_versor = sitk.VersorRigid3DTransform()
     rigid_versor.SetMatrix(rigid_euler.GetMatrix())
@@ -37,51 +86,89 @@ def rotate_and_resample(im, roll, pitch, yaw, show=False):
 
     if show:
         new_array = sitk.GetArrayFromImage(resampled)
-        fig, ax = plt.subplots(1, 3, figsize=(10, 3))
-        axial_slice_num = int(input('Enter axial slice number you wish to see:'))
-        coronal_slice_num = int(input('Enter coronal slice number:'))
-        sagittal_slice_num = int(input('Enter sagittal slice number:'))
-        ax[0].imshow(new_array[axial_slice_num])
-        ax[0].set_title('xy')
-        ax[1].imshow(new_array[:, coronal_slice_num, :], interpolation='nearest', aspect='auto')
-        ax[1].set_title('xz')
-        ax[2].imshow(new_array[:, :, sagittal_slice_num], interpolation='nearest', aspect='auto')
-        ax[2].set_title('yz')
+        fig = plt.figure()
+        plt.imshow(new_array[z_slice])
         plt.show()
 
     return resampled
 
 
-def main():
+def rotate_and_resample_2d(im, z_slice, angle, show=False):
+    im = im[:, :, z_slice]
+    arr = sitk.GetArrayFromImage(im)
+    y, x = np.nonzero(arr)
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+
+    # ROTATING
+    rotation_centre = (int(np.ceil(mean_x)), int(np.ceil(mean_y)))
+    rotation_centre_im = im.TransformIndexToPhysicalPoint(rotation_centre)
+
+    rigid_euler = sitk.Euler2DTransform()
+    rigid_euler.SetAngle(angle)
+    rigid_euler.SetCenter(rotation_centre_im)
+
+    # rigid_versor = sitk.VersorRigid2DTransform()
+    # rigid_versor.SetMatrix(rigid_euler.GetMatrix())
+    # rigid_versor.SetCenter(rotation_centre_im)
+
+    # RESAMPLING
+    interpolator = sitk.sitkNearestNeighbor
+    default_value = 0
+    resampled = sitk.Resample(
+        im,
+        im,
+        rigid_euler,
+        interpolator,
+        default_value
+    )
+
+    resampled = sitk.Cast(resampled, sitk.sitkFloat32)
+
+    if show:
+        new_array = sitk.GetArrayFromImage(resampled)
+        plt.imshow(new_array, cmap='gray')
+        # plt.set_title('xy')
+
+        plt.show()
+        plt.close()
+    return resampled
+
+
+def show_img(im, cmap):
+    new_array = sitk.GetArrayFromImage(im)
+    plt.imshow(new_array, cmap=cmap)
+    plt.axis('off')
+    plt.show()
+    plt.close()
+
+
+def main(im_path, mask_path, show_images=False, save_loc=None):
 
     # input the image plus its mask
-    im = sitk.ReadImage("../out_files/fixed_brain.nii.gz")
-    mask = sitk.ReadImage("../out_files/ls_bin_out.nii.gz")
+    im = sitk.ReadImage(im_path)
+    mask = sitk.ReadImage(mask_path, sitk.sitkUInt8)
 
-    # loss function for z-angle (YAW)
     def loss_func(angle, args):
-        pitch = angle[0]  # dont know why it passes as an array
-        roll = angle[1]
-        yaw = angle[2]
-        mask, z_slice, im = args
+        angle = angle[0]
+        z_slice, mask = args
         print('angle: {}'.format(angle))
-        mask_rotate = rotate_and_resample(mask, pitch, roll, yaw)
-        im_rotate = rotate_and_resample(im, pitch, roll, yaw)
-        im_slice = im_rotate[:, :, z_slice]
-        maxProj_mask_rotate = sitk.Cast(im_slice, 2) * sitk.Cast(mask_rotate[:, :, z_slice], 2)  # adjust
+        mask_rotate = rotate_and_resample_2d(mask, z_slice, angle)
         maxProj_flip = sitk.Flip(
-            maxProj_mask_rotate,
-            (True, False, False),
+            mask_rotate,
+            (True, False),
             flipAboutOrigin=True)
-        maxProj_flip.CopyInformation(maxProj_mask_rotate)
-        #    pix_count = np.count_nonzero(sitk.GetArrayFromImage(maxProj_mask_rotate - maxProj_flip))
-        subtArray = sitk.GetArrayFromImage(maxProj_mask_rotate - maxProj_flip)
-        pix_count = np.count_nonzero(subtArray > 50)
+        maxProj_flip.CopyInformation(mask_rotate)
+        subtArray = sitk.GetArrayFromImage(mask_rotate - maxProj_flip)
+        if show_images:
+            show_img(mask_rotate - maxProj_flip, 'Greens')
+        pix_count = np.count_nonzero(subtArray)
         print('pixel count: {}'.format(pix_count))
         return pix_count
 
+
     # initialize angle
-    angle_0 = [0, 0, 0]
+    angle_0 = [0]
 
     # calculate centre of mass for ls_bin_out to use as optimal slice location
     stats = sitk.LabelShapeStatisticsImageFilter()
@@ -93,16 +180,30 @@ def main():
     result = optimize.minimize(
         loss_func,
         angle_0,
-        args=[im, z_slice, mask],
+        args=[z_slice, mask],
         method='Powell',
-        bounds=[(-1, 1), (-1, 1), (-1, 1)])
+        bounds=[(-1, 1)])
 
     angle_final = result.x
 
-    resampled = rotate_and_resample(im, 0, 0, angle_final[0])
+    resampled = rotate_and_resample_yaw(im, z_slice, angle_final[0], show=False)
+    if show_images:
+        fig, axs = plt.subplots(1, 2, figsize=(6, 3))
+        for ax in axs.ravel():
+            ax.axis('off')
+        axs[0].imshow(sitk.GetArrayFromImage(im[:, :, z_slice]), cmap='gray')
+        axs[0].set_title('Original')
+        axs[1].imshow(sitk.GetArrayFromImage(resampled[:, :, z_slice]), cmap='gray')
+        axs[1].set_title('Result')
+        plt.show()
+        plt.close()
 
-    sitk.WriteImage(resampled, '../out_files/automatic_rotation.nii.gz')
+    if save_loc is not None:
+        sitk.WriteImage(resampled, os.path.join(save_loc, os.path.basename(im_path).split('.nii.gz')[0] + '_aligned.nii.gz'))
 
 
 if __name__ == '__main__':
-    main()
+
+    if len(sys.argv) < 2:
+        print("Usage: python image_3D_align.py path/to/image path/to/mask {show_images True or False} {save location}")
+    main(*sys.argv[1:])
