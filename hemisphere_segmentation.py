c474e5d1b814abbb010abc7590bca1f54af4eddf
FredaWerdiger <fwerdiger@gmail.com>
2023-08-14 12:32:49 +1000

    added png

--- hemisphere_segmentation.py
+++ hemisphere_segmentation.py
@@ -2,6 +2,7 @@
 import os
 import pandas as pd
 import sys
+
 sys.path.append('/data/gpfs/projects/punim1086/ctp_project/MONAI/')
 sys.path.append('../MONAI/')
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
@@ -39,7 +40,100 @@ from densenet import DenseNetFCN
 from sklearn.model_selection import train_test_split
 
 import torch
+import math
 import os
+
+def define_zvalues(ct_img):
+    z_min = int(ct_img.shape[2] * .25)
+    z_max = int(ct_img.shape[2] * .85)
+
+    steps = int((z_max - z_min) / 18)
+
+    if steps == 0:
+        z_min = 0
+        z_max = ct_img.shape[2]
+        steps = 1
+
+    z = list(range(z_min, z_max))
+
+    rem = int(len(z) / steps) - 18
+
+    if rem < 0:
+        add_on = [z[-1] for n in range(abs(rem))]
+        z.extend(add_on)
+    elif rem % 2 == 0:
+        z_min = z_min + int(rem / 2 * steps) + 1
+        z_max = z_max - int(rem / 2 * steps) + 1
+
+    elif rem % 2 != 0:
+        z_min = z_min + math.ceil(rem / 2)
+        z_max = z_max - math.ceil(rem / 2) + 1
+
+    z = list(range(z_min, z_max, steps))
+
+    if len(z) == 19:
+        z = z[1:]
+    elif len(z) == 20:
+        z = z[1:]
+        z = z[:18]
+
+    return z
+
+
+def create_image(ct_img,
+                 pred,
+                 savefile,
+                 z,
+                 ext='png',
+                 save=False,
+                 dpi=250):
+    ct_img, pred = [np.rot90(im) for im in [ct_img, pred]]
+    ct_img, pred = [np.fliplr(im) for im in [ct_img, pred]]
+    pred = np.where(pred == 0, np.nan, pred)
+
+    fig, axs = plt.subplots(6, 6, facecolor='k')
+    fig.subplots_adjust(hspace=-0.1, wspace=-0.3)
+    axs = axs.ravel()
+    for ax in axs:
+        ax.axis("off")
+    for i in range(6):
+        print(i)
+
+        axs[i].imshow(ct_img[:, :, z[i]], cmap='gray',
+                      interpolation='hanning', vmin=10, vmax=ct_img.max())
+        axs[i + 6].imshow(ct_img[:, :, z[i]], cmap='gray',
+                          interpolation='hanning', vmin=10, vmax=ct_img.max())
+        im = axs[i + 6].imshow(pred[:, :, z[i]], cmap='Reds', interpolation='hanning', alpha=0.5, vmin=0, vmax=1)
+
+    if 12 > len(z):
+        max2 = len(z)
+    else:
+        max2 = 12
+    for i in range(6, max2):
+        print(i)
+        axs[i + 6].imshow(ct_img[:, :, z[i]], cmap='gray',
+                          interpolation='hanning', vmin=10, vmax=ct_img.max())
+        axs[i + 12].imshow(ct_img[:, :, z[i]], cmap='gray',
+                           interpolation='hanning', vmin=10, vmax=ct_img.max())
+        im = axs[i + 12].imshow(pred[:, :, z[i]], cmap='Reds', interpolation='hanning', alpha=0.5, vmin=0, vmax=1)
+
+    if not 12 > len(z):
+        if len(z) > 18:
+            max3 = 18
+        else:
+            max3 = len(z)
+        for i in range(12, max3):
+            print(i)
+            axs[i + 12].imshow(ct_img[:, :, z[i]], cmap='gray',
+                               interpolation='hanning', vmin=10, vmax=ct_img.max())
+            axs[i + 18].imshow(ct_img[:, :, z[i]], cmap='gray',
+                               interpolation='hanning', vmin=10, vmax=ct_img.max())
+            axs[i + 18].imshow(pred[:, :, z[i]], cmap='Reds', interpolation='hanning', alpha=0.5, vmin=0, vmax=1)
+
+    if savefile:
+        plt.savefig(savefile, facecolor=fig.get_facecolor(), bbox_inches='tight', dpi=dpi, format=ext)
+        plt.close()
+
 
 def main():
     HOMEDIR = os.path.expanduser('~/')
@@ -57,7 +151,7 @@ def main():
                                 usecols=['subject', 'segmentation_type', 'dl_id'])
 
     data_dir = os.path.join(directory, 'DATA')
-    out_tag = 'left_hemisphere_mask'
+    out_tag = 'left_hemisphere_mask/densnet'
     all_image_paths = glob.glob(os.path.join(data_dir, 'ncct', '*'))
     all_image_paths.sort()
     mask_paths = glob.glob(os.path.join(data_dir, 'left_hemisphere_mask', '*'))
@@ -78,7 +172,7 @@ def main():
                                           random_state=random_state,
                                           shuffle=False)
     all_ids = [str(a).zfill(3) for a in range(len(all_image_paths))]
-    test_ids = [a for a in all_ids if not (a in val_ids+train_ids)]
+    test_ids = [a for a in all_ids if not (a in val_ids + train_ids)]
 
     def make_dict(id):
         paths1 = [file for file in all_image_paths
@@ -104,36 +198,36 @@ def main():
     val_interval = 2
 
     train_transforms = Compose(
-            [
-                LoadImaged(keys=["image", "label"]),
-                EnsureChannelFirstd(keys=["image", "label"]),
-                Resized(keys=["image", "label"],
-                        mode=['trilinear', "nearest"],
-                        align_corners=[True, None],
-                        spatial_size=[256]*3),
-                NormalizeIntensityd(keys=["image"], nonzero=True, channel_wise=True),
-                RandAffined(keys=['image', 'label'], prob=0.5, translate_range=10),
-                # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
-                # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
-                # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
-                RandScaleIntensityd(keys=["image"], factors=0.1, prob=1.0),
-                RandShiftIntensityd(keys=["image"], offsets=0.1, prob=1.0),
-                EnsureTyped(keys=["image", "label"]),
-            ]
-        )
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            Resized(keys=["image", "label"],
+                    mode=['trilinear', "nearest"],
+                    align_corners=[True, None],
+                    spatial_size=[256] * 3),
+            NormalizeIntensityd(keys=["image"], nonzero=True, channel_wise=True),
+            RandAffined(keys=['image', 'label'], prob=0.5, translate_range=10),
+            # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
+            # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
+            # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
+            RandScaleIntensityd(keys=["image"], factors=0.1, prob=1.0),
+            RandShiftIntensityd(keys=["image"], offsets=0.1, prob=1.0),
+            EnsureTyped(keys=["image", "label"]),
+        ]
+    )
 
     val_transforms = Compose(
-            [
-                LoadImaged(keys=["image", "label"]),
-                EnsureChannelFirstd(keys=["image", "label"]),
-                Resized(keys=["image", "label"],
-                        mode=['trilinear', "nearest"],
-                        align_corners=[True, None],
-                        spatial_size=[256]*3),
-                NormalizeIntensityd(keys=["image"], nonzero=True, channel_wise=True),
-                EnsureTyped(keys=["image", "label"]),
-            ]
-        )
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            Resized(keys=["image", "label"],
+                    mode=['trilinear', "nearest"],
+                    align_corners=[True, None],
+                    spatial_size=[256] * 3),
+            NormalizeIntensityd(keys=["image"], nonzero=True, channel_wise=True),
+            EnsureTyped(keys=["image", "label"]),
+        ]
+    )
 
     test_transforms = Compose(
         [
@@ -198,7 +292,6 @@ def main():
     # plt.axis('off')
     # plt.show()
     # plt.close()
-
 
     device = 'cuda'
     channels = (32, 64, 128, 256)
@@ -355,28 +448,30 @@ def main():
     pred_dir = os.path.join(directory + 'out_' + out_tag, "pred")
     if not os.path.exists(pred_dir):
         os.makedirs(pred_dir)
-
+    png_dir = os.path.join(directory + 'out_' + out_tag, "pngs")
+    if not os.path.exists(png_dir):
+        os.makedirs(png_dir)
 
     post_transforms = Compose([
-            EnsureTyped(keys=["pred"]),
-            Invertd(
-                keys=["pred"],
-                transform=test_transforms,
-                orig_keys=["image"],
-                meta_keys=["pred_meta_dict"],
-                orig_meta_keys=["image_meta_dict"],
-                meta_key_postfix="meta_dict",
-                nearest_interp=[False],
-                to_tensor=[True],
-            ),
-            AsDiscreted(keys="pred", argmax=True, to_onehot=2),
-            SaveImaged(
-                keys="pred",
-                meta_keys="pred_meta_dict",
-                output_dir=pred_dir,
-                output_postfix="seg",
-                resample=False,
-                separate_folder=False)
+        EnsureTyped(keys=["pred"]),
+        Invertd(
+            keys=["pred"],
+            transform=test_transforms,
+            orig_keys=["image"],
+            meta_keys=["pred_meta_dict"],
+            orig_meta_keys=["image_meta_dict"],
+            meta_key_postfix="meta_dict",
+            nearest_interp=[False],
+            to_tensor=[True],
+        ),
+        AsDiscreted(keys="pred", argmax=True, to_onehot=2),
+        SaveImaged(
+            keys="pred",
+            meta_keys="pred_meta_dict",
+            output_dir=pred_dir,
+            output_postfix="seg",
+            resample=False,
+            separate_folder=False)
     ])
 
     dice_metric = DiceMetric(include_background=False, reduction="mean")
@@ -384,7 +479,7 @@ def main():
     device = 'cpu' if not torch.cuda.is_available() else 'cuda'
 
     model.load_state_dict(torch.load(os.path.join(
-                        directory, 'out_' + out_tag, model_path)))
+        directory, 'out_' + out_tag, model_path)))
 
     model.eval()
 
@@ -404,11 +499,14 @@ def main():
 
             original_image = loader(test_data[0]["image_meta_dict"]["filename_or_obj"])
             original_image = original_image[0]  # image data
-            original_image = original_image[:, :, :, 0]
             prediction = test_output[0][1].detach().numpy()
             name = os.path.basename(
                 test_data[0]["image_meta_dict"]["filename_or_obj"]).split('.nii.gz')[0].split('_')[1]
             subject = ctp_dl_df.loc[[name], "subject"].values[0]
+            save_loc = png_dir + '/' + name + '_pred.png'
+
+            create_image(original_image, prediction, save_loc,
+                                     define_zvalues(original_image), ext='png', save=True)
             # results.loc[results.id == name, 'dice'] = dice_score
             # results.loc[results.id == name, 'subject'] = subject
         # # aggregate the final mean dice result
@@ -420,6 +518,7 @@ def main():
     # results['mean_dice'] = metric
     # print(results)
     # results.to_csv(directory + 'out_' + out_tag + '/results.csv', index=False)
+
 
 if __name__ == '__main__':
     main()
